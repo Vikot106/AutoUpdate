d8922b7d4a5e2ead1b79e3001e80419e7259f964
lihangrui <1789157552@qq.com>
2021-03-05 09:53:31 +0800

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace AutoUpdate {
+    partial class Form1 {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent() {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(351, 60);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "选择启动器文件";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(24, 59);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(303, 25);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(330, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "选择 泯默1.16.5整合包 下的“HMCL.exe”文件";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Enabled = false;
+            this.checkBox1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.checkBox1.Location = new System.Drawing.Point(24, 120);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(194, 19);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "（已选）必要的配置文件";
+            this.checkBox1.UseVisualStyleBackColor = false;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Location = new System.Drawing.Point(24, 155);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(393, 19);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "（推荐）WorldMap大地图附属（需自行修改按键设定）";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(24, 190);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(285, 19);
+            this.checkBox3.TabIndex = 5;
+            this.checkBox3.Text = "（可选）InventoryProfiles背包整理";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(24, 226);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(213, 19);
+            this.checkBox4.TabIndex = 6;
+            this.checkBox4.Text = "（可选）AutoFish自动钓鱼";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(167, 330);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(160, 45);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "更新";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 274);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(210, 15);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "*可选模组使用方法需自行查阅";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 407);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "|AutoUpdate| by 泯默 Ver.20210304";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Reflection;
+
+namespace AutoUpdate {
+    public partial class Form1 : Form {
+        public string file = "";
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e) {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Multiselect = false;//该值确定是否可以选择多个文件
+            dialog.Title = "请选“HMCL.exe”";
+            dialog.Filter = "HMCL Launcher(*.exe)|*.exe";
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK) {
+                this.file = dialog.FileName;
+                textBox1.Text = this.file;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            string path = "";
+            if (this.file.Equals("")) {
+                MessageBox.Show("你还没选择启动器文件路径呢");
+            }else {
+                var strArray = this.file.Split('\\');
+                for(int i = 0; i < strArray.Length - 1; i++) {
+                    path = path + strArray[i] + '\\';
+                }
+                string cfgPath = path + ".minecraft\\versions\\1.16.5\\CustomSkinLoader";
+                string modPath = path + ".minecraft\\versions\\1.16.5\\mods";
+                try {
+                    if (File.Exists(cfgPath + "\\CustomSkinLoader.json")) {
+                        File.Delete(cfgPath + "\\CustomSkinLoader.json");
+                    }
+                    byte[] buff1 = Properties.Resources.CustomSkinLoader;
+                    File.WriteAllBytes(cfgPath + "\\CustomSkinLoader.json", buff1);
+                    if (checkBox2.Checked) {
+                        byte[] buff2 = Properties.Resources.XaerosWorldMap_1_11_11_2_Forge_1_16_5;
+                        File.WriteAllBytes(modPath + "\\XaerosWorldMap_1.11.11.2_Forge_1.16.5.jar", buff2);
+                    }
+                    if (checkBox3.Checked) {
+                        byte[] buff3 = Properties.Resources.inventoryprofiles_forge_1_16_2_0_4_2;
+                        File.WriteAllBytes(modPath + "\\inventoryprofiles-forge-1.16.2-0.4.2.jar", buff3);
+                    }
+                    if (checkBox4.Checked) {
+                        byte[] buff4 = Properties.Resources.forgeautofish_2_0_3_1_16_x;
+                        File.WriteAllBytes(modPath + "\\forgeautofish-2.0.3-1.16.x.jar", buff4);
+                    }
+                    MessageBox.Show("更新完了 溜了");
+                    System.Environment.Exit(0);
+                }
+                catch (Exception) {
+                    MessageBox.Show("更新失败，请检查路径或文件占用情况");
+                }
+            }
+        }
+    }
+}
